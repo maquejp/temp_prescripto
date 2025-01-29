9c3304bdedf7c26b956c5754c8f4990fa501c324
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-01-29 15:29:29 +0100

    feature:home: header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <Header />
+  </div>;
 };
 
 export default Home;
