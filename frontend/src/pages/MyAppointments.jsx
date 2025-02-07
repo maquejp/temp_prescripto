9d42d7ca2b2a0872098eb22795d15d5b13af3220
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-02-07 16:34:37 +0100

    styling

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -12,25 +12,27 @@ const MyAppointments = () => {
         {doctors.slice(0, 3).map((doc, index) => (
           <div
             key={index}
-            className="border border-gray-400 rounded-lg my-4 py-4 px-4 flex items-center gap-4"
+            className="border border-gray-400 rounded-lg my-4 py-4 px-4 flex md:flex-col items-center gap-4"
           >
-            <img src={doc.image} alt="docimg" className="w-50" />
             <div className="w-full">
-              <p className="font-semibold">{doc.name}</p>
-              <p>{doc.speciality}</p>
+              <img src={doc.image} alt="docimg" className="w-50" />
+              <div className="w-full">
+                <p className="font-semibold">{doc.name}</p>
+                <p>{doc.speciality}</p>
 
-              <div className="pt-4">
-                <p className="font-semibold">Address</p>
-                <p>{doc.address.line1}</p>
-                <p>{doc.address.line2}</p>
-              </div>
+                <div className="pt-4">
+                  <p className="font-semibold">Address</p>
+                  <p>{doc.address.line1}</p>
+                  <p>{doc.address.line2}</p>
+                </div>
 
-              <div className="flex items-center gap-4 pt-4">
-                <p className="font-semibold">Date & Time:</p>
-                <p> 25, may 2023 | 10:00 AM</p>
+                <div className="flex items-center gap-4 pt-4">
+                  <p className="font-semibold">Date & Time:</p>
+                  <p> 25, may 2023 | 10:00 AM</p>
+                </div>
               </div>
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col justify-end">
               <button className="bg-app-primary py-1 px-20 mt-1 rounded-full text-white text-sm cursor-pointer">
                 Pay
               </button>
