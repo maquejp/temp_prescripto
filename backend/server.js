21fa1c4ff2a1e43e9684d40da8984e407d2b611b
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-02-07 16:45:30 +0100

    backend: initial

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api end points
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
